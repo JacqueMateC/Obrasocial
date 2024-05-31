15d0e969f3e6efd3c16b3da533cb1d7edcb05c1d
Jaque <166718359+JacqueMateC@users.noreply.github.com>
2024-05-31 15:31:55 -0300

    Update script.js
    Signed-off-by: Jaque <166718359+JacqueMateC@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Simulación de obtención de datos de la CDC
-    const dataFromCDC = [
+    // Datos de los enlaces a mostrar
+    const data = [
         { title: "Prevención de la Gripe", link: "https://www.cdc.gov/flu/prevent/index.html" },
-        { title: "Información sobre el COVID-19", link: "https://www.cdc.gov/coronavirus/2019-ncov/index.html" }
+        { title: "Información sobre el COVID-19", link: "https://www.cdc.gov/coronavirus/2019-ncov/index.html" },
+        { title: "PAMI Vademecum", link: "https://servicios.pami.org.ar/vademecum/views/consultaPublica/listado.zul" }
     ];
 
-    const novedadesSection = document.getElementById('novedades');
-    const cdcContainer = document.createElement('div');
-    cdcContainer.innerHTML = '<h3>Recursos de la CDC</h3>';
+    // Contenedor de novedades
+    const novedadesSection1 = document.getElementById('contenido-iframe-1');
+    const iframe1 = document.createElement('iframe');
+    iframe1.src = data[0].link;
+    iframe1.width = "100%";
+    iframe1.height = "600px";
+    iframe1.style.border = "none";
+    novedadesSection1.appendChild(iframe1);
 
-    const list = document.createElement('ul');
-    dataFromCDC.forEach(item => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `<a href="${item.link}" target="_blank">${item.title}</a>`;
-        list.appendChild(listItem);
-    });
-
-    cdcContainer.appendChild(list);
-    novedadesSection.appendChild(cdcContainer);
-
-    // Cargar iframe con el contenido del enlace proporcionado
-    const iframeContainer = document.getElementById('contenido-iframe');
-    const iframe = document.createElement('iframe');
-    iframe.src = "https://servicios.pami.org.ar/vademecum/views/consultaPublica/listado.zul";
-    iframe.width = "100%";
-    iframe.height = "600px"; // Ajusta la altura según sea necesario
-    iframe.style.border = "none";
-    iframeContainer.appendChild(iframe);
+    const novedadesSection2 = document.getElementById('contenido-iframe-2');
+    const iframe2 = document.createElement('iframe');
+    iframe2.src = data[2].link;
+    iframe2.width = "100%";
+    iframe2.height = "600px";
+    iframe2.style.border = "none";
+    novedadesSection2.appendChild(iframe2);
 });
